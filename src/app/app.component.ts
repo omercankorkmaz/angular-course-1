3ab6b11bcf864c3d6b7be0b7d13716a93dd10ea6
Ömer Can Korkmaz <omercankorkmaz0@gmail.com>
2022-02-11 14:54:44 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Inject, Injector, OnInit} from '@angular/core';
+import {AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, Injector, OnChanges, OnDestroy, OnInit, QueryList, SimpleChanges, ViewChild, ViewChildren} from '@angular/core';
 import {Course} from './model/course';
 import {Observable} from 'rxjs';
 import {AppConfig, CONFIG_TOKEN} from './config';
@@ -6,6 +6,7 @@ import {COURSES} from '../db-data';
 import {CoursesService} from './courses/courses.service';
 import {createCustomElement} from '@angular/elements';
 import {CourseTitleComponent} from './course-title/course-title.component';
+import { CourseCardComponent } from './courses/course-card/course-card.component';
 
 
 @Component({
@@ -13,35 +14,68 @@ import {CourseTitleComponent} from './course-title/course-title.component';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnChanges, OnInit, AfterContentInit, AfterViewInit {
 
     courses: Course[] = COURSES;
 
     coursesTotal = this.courses.length;
 
+    @ViewChild(CourseCardComponent, { read: ElementRef })
+    cardElementRef: ElementRef;
+
+    @ViewChild(CourseCardComponent)
+    cardComponent: CourseCardComponent;
+
+    @ViewChildren(CourseCardComponent, { read: ElementRef })
+    cardElementRefs: QueryList<ElementRef>;
+
+    @ViewChildren(CourseCardComponent)
+    cardComponents: QueryList<CourseCardComponent>;
+
+    bool: boolean;
+
     constructor(
         private coursesService: CoursesService,
         @Inject(CONFIG_TOKEN) private config: AppConfig,
         private injector: Injector) {
-
     }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        // Gets invoked whenever the input value gets changed.
+    }
     ngOnInit() {
-
         const htmlElement = createCustomElement(CourseTitleComponent, {injector:this.injector});
-
-        customElements.define('course-title', htmlElement);
-
+        customElements.define('course-title', htmlElement);        
     }
+    ngAfterContentInit() {
+        this.courses[1].description = 'test';
+        this.bool = true;
+    }
+    ngAfterViewInit(): void {
+        // console.log(this.cardElementRef);
+        // console.log(this.cardComponent);
+        // console.log(this.cardElementRefs);
+        // console.log(this.cardComponents);
+        // this.courses[1].description = 'test'; // ExpressionChangedAfterItHasBeenCheckedError
+        // this.bool = true; // no error
+
+        // this.cardComponents.changes.subscribe(res => {
+        //     console.log(res);
+        // })
+        // this.cardElementRefs.changes.subscribe(res => {
+        //     console.log(res);
+        // })
+    }
+
+
+
 
     onEditCourse() {
-
-            this.courses[1].category = 'ADVANCED';
-
+        this.courses[1].category = 'ADVANCED';
+        this.courses.push(undefined); // triggers .changes.subs
     }
 
     save(course: Course) {
-
         this.coursesService.saveCourse(course)
             .subscribe(
                 () => console.log('Course Saved!')
--- src/app/courses/course-card/course-card.component.ts
+++ src/app/courses/course-card/course-card.component.ts
@@ -1,16 +1,14 @@
 import {
-    AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
     Attribute,
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component, DoCheck,
+    Component,
     EventEmitter,
-    Input, OnChanges,
-    OnDestroy,
+    Input,
     OnInit,
-    Output
+    Output,
+    AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, DoCheck, ElementRef, Inject, Injector, OnChanges, OnDestroy, SimpleChanges, ViewChild, ContentChild, TemplateRef
 } from '@angular/core';
 import {Course} from '../../model/course';
+import { CourseImageComponent } from '../course-image/course-image.component';
 import {CoursesService} from '../courses.service';
 
 
@@ -20,7 +18,17 @@ import {CoursesService} from '../courses.service';
     templateUrl: './course-card.component.html',
     styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent implements  OnInit {
+export class CourseCardComponent implements  OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+ 
+    // ViewChild ng-content'in icindekini goremez so;
+
+    @ContentChild(CourseImageComponent)
+    courseImageComponent: CourseImageComponent;
+
+    @ContentChild(CourseImageComponent, { read: ElementRef })
+    courseImageElementRef: ElementRef;
+
+    // ContentChildren is also same as ViewChildren;
 
     @Input()
     course: Course;
@@ -31,20 +39,44 @@ export class CourseCardComponent implements  OnInit {
     @Output('courseChanged')
     courseEmitter = new EventEmitter<Course>();
 
-    dontShowImg: boolean = false;
-
+    @Input()
+    noImageTemplate: TemplateRef<any>;
 
     constructor(private coursesService: CoursesService,
                 @Attribute('type') private type: string) {
-
-
+        
     }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        // Gets invoked whenever the input value gets changed.
+    }
     ngOnInit() {
 
     }
+    ngDoCheck(): void {
+
+    }
+    ngAfterContentInit(): void {
+        // External child components can be included by Angular using this method within the <ng-content> </ng-content> tag
+    }
+    ngAfterContentChecked() {
+        // It plays a big role in the initialization of the child component.
+    }
+    ngAfterViewInit(): void {
+        // do not modify the data that component try to render
+    }
+    ngAfterViewChecked(): void {
+        // When something is awaited from the child component, this component can be helpful.
+    }
+    ngOnDestroy(): void {
+        // This is the clean-up phase just use before Angular destroys the directive/component
+    }
+
+    // 1 2 3 4 5 6 7 3 5 7
 
 
+    // ViewChild    => earliest access -> ngAfterViewInit
+    // ContentChild => earliest access -> ngAfterContentInit
 
     onTitleChanged(newTitle: string) {
 
@@ -68,7 +100,6 @@ export class CourseCardComponent implements  OnInit {
 
     getCourseCardStyles() {
         if (this.course.category === 'BEGINNER') {
-            this.dontShowImg = true;
             return {
                 'background-image': `url(${this.course.iconUrl})`
             }
